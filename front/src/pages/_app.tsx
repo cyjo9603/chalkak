8d8e084b9a318bcbd79027daf544c3dd12b1b2ad
Chanyeong Cho <cyjo9603@naver.com>
2020-03-25 21:42:03 +0900

    [feat] set styled theme

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -7,10 +7,12 @@ import withReduxSaga from 'next-redux-saga';
 import createSagaMiddleware from 'redux-saga';
 import { createStore, Store, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 import axios from 'axios';
 
 import reducer from '../reducers';
 import rootSaga from '../sagas/index';
+import theme from '../theme';
 
 interface Props {
   Component: AppProps['Component'];
@@ -22,32 +24,34 @@ const Chalkak = ({ Component, store, pageProps }: Props) => {
   return (
     <Container>
       <Provider store={store}>
-        <Helmet>
-          <title>chalkak</title>
-          <meta charSet="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover"
-          />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="description" content="Chalkak" />
-          <meta name="og:title" content="Chalkak" />
-          <meta name="og:description" content="Chalkak" />
-          <meta property="og:type" content="website" />
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css" />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-          />
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.js"></script>
-        </Helmet>
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Helmet>
+            <title>chalkak</title>
+            <meta charSet="UTF-8" />
+            <meta
+              name="viewport"
+              content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover"
+            />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="description" content="Chalkak" />
+            <meta name="og:title" content="Chalkak" />
+            <meta name="og:description" content="Chalkak" />
+            <meta property="og:type" content="website" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css" />
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+            />
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+            />
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.js"></script>
+          </Helmet>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </Provider>
     </Container>
   );
