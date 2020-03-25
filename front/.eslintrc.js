2a672218386a92a8a18ac04c2bf8ed434788b499
Chanyeong Cho <cyjo9603@naver.com>
2020-03-25 17:34:37 +0900

    [env] init env setting

---
+++ front/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: [
+    'airbnb-base',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
+    'linebreak-style': 0,
+    'import/extensions': 0,
+    'no-underscore-dangle': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
