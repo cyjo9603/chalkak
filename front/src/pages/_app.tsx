c04fb62e148edd61159235b058061700b0a97fc3
Chanyeong Cho <cyjo9603@naver.com>
2020-03-26 18:13:26 +0900

    [chore] add eslint rule

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import Helmet from 'react-helmet';
 import withRedux, { MakeStoreOptions } from 'next-redux-wrapper';
