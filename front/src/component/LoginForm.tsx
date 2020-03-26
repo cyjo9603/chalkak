ab8b6b5020bc51dcf5084660263e300e723cbe54
Chanyeong Cho <cyjo9603@naver.com>
2020-03-26 22:41:41 +0900

    [feat] add signup button

--- front/src/component/LoginForm.tsx
+++ front/src/component/LoginForm.tsx
@@ -9,6 +9,7 @@ const LoginForm = () => {
       <Input placeholder="아이디" prefix={<UserOutlined />} />
       <Input.Password placeholder="비밀번호" />
       <Button ghost>로그인</Button>
+      <Button ghost>회원가입</Button>
     </FormWrapper>
   );
 };
@@ -16,8 +17,13 @@ const LoginForm = () => {
 const FormWrapper = styled.div`
   display: flex;
 
-  & > span:nth-child(-n + 2) {
+  & > span {
     width: 150px;
+    margin-right: 10px;
+  }
+
+  & > button {
+    width: 100px;
     margin-right: 10px;
   }
 `;
