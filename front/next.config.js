6d825a3d186ed258bf6f782e4414512987f4e343
Chanyeong Cho <cyjo9603@naver.com>
2020-03-28 17:56:26 +0900

    [env] set entd less

--- front/next.config.js
+++ front/next.config.js
@@ -1,7 +1,20 @@
-const withTypescript = require('@zeit/next-typescript');
+const withLess = require('@zeit/next-less');
+const lessToJS = require('less-vars-to-js');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = withTypescript({
-  webpack: config => {
+const themeVariables = lessToJS(fs.readFileSync(path.resolve(__dirname, './src/less/antd-custom.less'), 'utf8'));
+
+if (typeof require !== 'undefined') {
+  require.extensions['.less'] = (file) => {};
+}
+
+module.exports = withLess({
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+    modifyVars: themeVariables,
+  },
+  webpack: (config) => {
     return config;
   },
 });
