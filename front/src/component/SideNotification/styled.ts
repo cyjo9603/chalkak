5abac4d26151459a12815b58d8d4c461b3b15fc9
Chanyeong Cho <cyjo9603@naver.com>
2020-04-04 16:50:06 +0900

    [style] change side nodification style

--- front/src/component/SideNotification/styled.ts
+++ front/src/component/SideNotification/styled.ts
@@ -5,4 +5,9 @@ export default styled(Card)`
   width: calc(100% - 10px);
   border-radius: 15px;
   margin: 0 0 10px 10px;
+
+  @media (max-width: ${(props) => props.theme.BREAK_POINT.TABLET}px) {
+    width: 100%;
+    margin-left: 0;
+  }
 `;
