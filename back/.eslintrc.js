92f9bbad9232828c6b4b6bf5236dc28f3c48c399
Chanyeong Cho <cyjo9603@naver.com>
2020-04-06 17:04:09 +0900

    [env] init env setting

---
+++ back/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/eslint-recommended'],
+  rules: {
+    'linebreak-style': 0,
+  },
+};
