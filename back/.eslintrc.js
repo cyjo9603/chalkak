b2196871fdad79409e7d28dda64944b6f4d48d45
Chanyeong Cho <cyjo9603@naver.com>
2020-04-06 23:20:42 +0900

    [env] add rules

--- back/.eslintrc.js
+++ back/.eslintrc.js
@@ -4,5 +4,7 @@ module.exports = {
   extends: ['airbnb-base', 'plugin:@typescript-eslint/eslint-recommended'],
   rules: {
     'linebreak-style': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
   },
 };
