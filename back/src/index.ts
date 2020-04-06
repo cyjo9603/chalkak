7909b61af4a05b361076c7b56c2b3a3730609e9a
Chanyeong Cho <cyjo9603@naver.com>
2020-04-06 17:04:26 +0900

    [feat] init setting

---
+++ back/src/index.ts
@@ -0,0 +1,58 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import expressSession from 'express-session';
+import dotenv from 'dotenv';
+import passport from 'passport';
+import hpp from 'hpp';
+import helmet from 'helmet';
+
+dotenv.config();
+
+const app = express();
+const prod = process.env.NODE_ENV === 'production';
+
+app.set('port', prod ? process.env.PORT : 3065);
+
+if (prod) {
+  app.use(hpp());
+  app.use(helmet());
+  app.use(morgan('combined'));
+  app.use(
+    cors({
+      origin: /domainname\.com/,
+      credentials: true,
+    }),
+  );
+} else {
+  app.use(morgan('dev'));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    }),
+  );
+}
+
+app.use('/', express.static('uploads'));
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET!,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      domain: prod ? 'domainname.com' : undefined,
+    },
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.listen(app.get('port'), () => {
+  console.log(`server is running on ${app.get('port')}`);
+});
