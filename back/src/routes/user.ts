2895ba910a92aa47093d0433cd6b5b86fd5a527e
Chanyeong Cho <cyjo9603@naver.com>
2020-04-07 22:51:40 +0900

    [feat] add api: id check

--- back/src/routes/user.ts
+++ back/src/routes/user.ts
@@ -1,9 +1,25 @@
 import express from 'express';
 
 import User from '../sequelize/models/user';
+import { isNotLoggedIn } from './middleware';
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {});
+
+router.post('/idcheck', isNotLoggedIn, async (req, res, next) => {
+  try {
+    const exUser = await User.findOne({
+      where: {
+        userId: req.body.userId,
+      },
+      attributes: ['userId'],
+    });
+    return res.json({ exUser });
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
 
 export default router;
