e5a2b0d0554069fedd711b962ec3d12c837367a9
Chanyeong Cho <cyjo9603@naver.com>
2020-04-07 23:02:58 +0900

    [feat] add api: signup

--- back/src/routes/user.ts
+++ back/src/routes/user.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 
 import User from '../sequelize/models/user';
 import { isNotLoggedIn } from './middleware';
@@ -16,6 +17,35 @@ router.post('/idcheck', isNotLoggedIn, async (req, res, next) => {
       attributes: ['userId'],
     });
     return res.json({ exUser });
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+
+router.post('/signup', isNotLoggedIn, async (req, res, next) => {
+  try {
+    const exUser = await User.findOne({
+      where: {
+        userId: req.body.userId,
+      },
+      attributes: ['userId'],
+    });
+    if (exUser) {
+      return res.status(400).send('이미 사용중인 아이디입니다.');
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+
+    await User.create({
+      familyName: req.body.familyName,
+      firstName: req.body.firstName,
+      userId: req.body.userId,
+      password: hashedPassword,
+      phone: req.body.phone,
+      mail: req.body.mail,
+    });
+
+    return res.json({ result: 'success' });
   } catch (e) {
     console.error(e);
     next(e);
