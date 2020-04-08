9c45fbf81fd66446e55b86f3eff9f81f22214894
Chanyeong Cho <cyjo9603@naver.com>
2020-04-08 20:16:00 +0900

    [feat] add api: request friend

--- back/src/routes/notify.ts
+++ back/src/routes/notify.ts
@@ -1,9 +1,31 @@
 import express from 'express';
 
 import Notify from '../sequelize/models/notify';
+import User from '../sequelize/models/user';
+import { isLoggedIn } from './middleware';
+
+const REQUEST_FRIEND = 'REQUEST_FRIEND' as const;
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {});
+router.post('/friend/request', isLoggedIn, async (req, res) => {
+  const { id } = req.user as User;
+  const targetId = await User.findOne({
+    where: {
+      userId: req.body.id,
+    },
+    attributes: ['id'],
+  });
+
+  const exRequest = await Notify.findOrCreate({
+    where: {
+      notifyType: REQUEST_FRIEND,
+      requestorId: id,
+      targetId: targetId.id,
+    },
+  });
+
+  return res.json(exRequest);
+});
 
 export default router;
