462358d104de0516515e9433a1737f7afe5a6844
Chanyeong Cho <cyjo9603@naver.com>
2020-04-08 14:09:03 +0900

    [feat] set multer : image uploads

--- back/src/routes/post.ts
+++ back/src/routes/post.ts
@@ -1,8 +1,27 @@
 import express from 'express';
+import multer from 'multer';
+import path from 'path';
 
 import Post from '../sequelize/models/post';
 
 const router = express.Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, 'uploads');
+    },
+
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      const basename = path.basename(file.originalname);
+      done(null, basename + new Date().valueOf() + ext);
+    },
+  }),
+  limits: {
+    fileSize: 20 * 1024 * 1024,
+  },
+});
 
 router.get('/', async (req, res, next) => {});
 
