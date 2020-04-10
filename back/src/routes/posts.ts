090cf8a1e58e1975aff2e8eccd77dc39a46c9a7f
Chanyeong Cho <cyjo9603@naver.com>
2020-04-10 15:17:57 +0900

    [feat] add api: get all posts

--- back/src/routes/posts.ts
+++ back/src/routes/posts.ts
@@ -1,9 +1,62 @@
 import express from 'express';
+import { Op } from 'sequelize';
 
 import Post from '../sequelize/models/post';
+import User from '../sequelize/models/user';
+import Image from '../sequelize/models/image';
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {});
+router.get('/', async (req, res, next) => {
+  try {
+    let where = {};
+    if (req.query.lastUpdatedAt) {
+      where = {
+        updatedAt: {
+          [Op.lt]: new Date(req.query.lastUpdatedAt),
+        },
+      };
+    }
+
+    const posts = await Post.findAll({
+      where,
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'familyName', 'firstName'],
+        },
+        {
+          model: Image,
+        },
+        {
+          model: User,
+          as: 'Likers',
+          attributes: ['id'],
+        },
+        {
+          model: Post,
+          as: 'SharePost',
+          include: [
+            {
+              model: User,
+              attributes: ['id', 'familyName', 'firstName'],
+            },
+            {
+              model: Image,
+            },
+          ],
+        },
+      ],
+
+      order: [['updatedAt', 'DESC']],
+      limit: parseInt(req.query.limit, 10),
+    });
+
+    res.json(posts);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
 
 export default router;
