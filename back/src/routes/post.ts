f3237c4821e1c3d2c07c86d7f4383a28bd0bd722
Chanyeong Cho <cyjo9603@naver.com>
2020-04-10 15:55:17 +0900

    [feat] add api: unlike post

--- back/src/routes/post.ts
+++ back/src/routes/post.ts
@@ -106,4 +106,26 @@ router.post('/:id/like', isLoggedIn, async (req, res, next) => {
   }
 });
 
+router.delete('/:id/like', isLoggedIn, async (req, res, next) => {
+  try {
+    const { id } = req.user as User;
+    const post = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!post) {
+      res.status(404).send('포스트가 존재하지 않습니다.');
+    }
+
+    await post.removeLiker(id);
+
+    return res.json({ userId: id });
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+
 export default router;
