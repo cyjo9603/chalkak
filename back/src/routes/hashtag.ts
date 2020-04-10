ce4746d2344493a77e04998a63ea79c327f6a9dc
Chanyeong Cho <cyjo9603@naver.com>
2020-04-10 15:39:29 +0900

    [feat] add api: get hashtag posts

--- back/src/routes/hashtag.ts
+++ back/src/routes/hashtag.ts
@@ -1,9 +1,67 @@
 import express from 'express';
+import { Op } from 'sequelize';
 
 import Hashtag from '../sequelize/models/hashtag';
+import Post from '../sequelize/models/post';
+import User from '../sequelize/models/user';
+import Image from '../sequelize/models/image';
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {});
+router.get('/:tag', async (req, res, next) => {
+  try {
+    let where = {};
+    if (req.query.lastUpdatedAt) {
+      where = {
+        updatedAt: {
+          [Op.lt]: new Date(req.query.lastUpdatedAt),
+        },
+      };
+    }
+    const posts = await Post.findAll({
+      where,
+      include: [
+        {
+          model: Hashtag,
+          where: {
+            name: decodeURIComponent(req.params.tag),
+          },
+        },
+        {
+          model: User,
+          attributes: ['id', 'familyName', 'firstName'],
+        },
+        {
+          model: Image,
+        },
+        {
+          model: User,
+          as: 'Likers',
+          attributes: ['id'],
+        },
+        {
+          model: Post,
+          as: 'SharePost',
+          include: [
+            {
+              model: User,
+              attributes: ['id', 'familyName', 'firstName'],
+            },
+            {
+              model: Image,
+            },
+          ],
+        },
+      ],
+      order: [['updatedAt', 'DESC']],
+      limit: parseInt(req.query.limit, 10),
+    });
+
+    return res.json(posts);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
 
 export default router;
