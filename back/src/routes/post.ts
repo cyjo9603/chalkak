15c0d64966cafe9244facf890bd2eb60f148b63a
Chanyeong Cho <cyjo9603@naver.com>
2020-04-10 16:15:55 +0900

    [feat] add api: write comment

--- back/src/routes/post.ts
+++ back/src/routes/post.ts
@@ -7,6 +7,7 @@ import User from '../sequelize/models/user';
 import { isLoggedIn } from './middleware';
 import Hashtag from '../sequelize/models/hashtag';
 import Image from '../sequelize/models/image';
+import Comment from '../sequelize/models/comment';
 
 const router = express.Router();
 
@@ -146,6 +147,46 @@ router.delete('/:id/like', isLoggedIn, async (req, res, next) => {
     await post.removeLiker(id);
 
     return res.json({ userId: id });
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+
+router.post('/:id/comment', isLoggedIn, async (req, res, next) => {
+  try {
+    const { id } = req.user as User;
+    const post = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!post) {
+      return res.status(404).send('포스트가 존재하지 않습니다.');
+    }
+
+    const newComment = await Comment.create({
+      PostId: post.id,
+      UserId: id,
+      content: req.body.content,
+    });
+
+    await post.addComment(newComment.id);
+
+    const comment = await Comment.findOne({
+      where: {
+        id: newComment.id,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'familyName', 'firstName'],
+        },
+      ],
+    });
+
+    return res.json(comment);
   } catch (e) {
     console.error(e);
     next(e);
