d58546c697ff5bd2fa26507fe8c23529a4f625d8
Chanyeong Cho <cyjo9603@naver.com>
2020-04-11 15:40:05 +0900

    [feat] set user route initial state

--- front/src/reducers/user/index.ts
+++ front/src/reducers/user/index.ts
@@ -1,11 +1,67 @@
 import produce from 'immer';
 
+export const NOTIFY_REQUEST_FRIEND = 'NOTIFY_REQUEST_FRIEND' as const;
+export const NOTIFY_WELCOME = 'NOTIFY_WELCOME' as const;
+export const NOTIFY_POST_COMMENTS = 'NOTIFY_POST_COMMENTS' as const;
+
+export type notifyType = typeof NOTIFY_REQUEST_FRIEND | typeof NOTIFY_WELCOME | typeof NOTIFY_POST_COMMENTS;
+
+export interface UserInfo {
+  id: number;
+  familyName: string;
+  firstName: string;
+  userId: string;
+  birth: string;
+  phone: string;
+  mail: string;
+  profilePhoto: string;
+  friends: number;
+}
+
+export interface UserNotify {
+  id: number;
+  notifyType: notifyType;
+  requestorId: number;
+  requestor: {
+    familyName: string;
+    firstName: string;
+  };
+}
+
+export interface UserFriends {
+  id: number;
+  familyName: string;
+  firstName: string;
+  profilePthoto: string;
+  Friend: {
+    createdAt: string;
+    updatedAt: string;
+    UserId: number;
+    FriendId: number;
+  };
+}
+
+export interface OtherUserInfo {
+  id: number;
+  familyName: string;
+  firstName: string;
+  profilePhoto?: string;
+  Posts: number;
+  Friends: number;
+}
+
 export interface UserInitialState {
-  init: null;
+  info: UserInfo | null;
+  notify: UserNotify[] | null;
+  Friends: UserFriends[] | null;
+  otherUserInfo: UserInitialState | null;
 }
 
 const initialState: UserInitialState = {
-  init: null,
+  info: null,
+  notify: null,
+  Friends: null,
+  otherUserInfo: null,
 };
 
 const user = (state: UserInitialState = initialState, action: any) => {
