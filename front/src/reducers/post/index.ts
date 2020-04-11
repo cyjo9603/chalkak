276a82bdca83052e3ed5bae25e6fc1e2fcce8f1c
Chanyeong Cho <cyjo9603@naver.com>
2020-04-11 17:01:24 +0900

    [feat] set post route initial state

--- front/src/reducers/post/index.ts
+++ front/src/reducers/post/index.ts
@@ -1,11 +1,66 @@
 import produce from 'immer';
 
+export interface PostImage {
+  id: number;
+  src: string;
+}
+
+export interface PostUserInfo {
+  id: number;
+  familyName: string;
+  firstName: string;
+  profilePhoto: string | null;
+}
+
+export interface PostLiker {
+  id: number;
+  Like: {
+    createdAt: string;
+    updatedAt: string;
+    PostId: number;
+    UserId: number;
+  };
+}
+
+export interface CommentInfo {
+  id: number;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  PostId: number;
+  User: PostUserInfo;
+}
+
+export interface SharePostInfo {
+  id: number;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  User: PostUserInfo;
+  Images: PostImage;
+}
+
+export interface PostInfo {
+  id: number;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  User: PostUserInfo;
+  Images: PostImage[];
+  Likers: PostLiker[];
+  SharePostId: number | null;
+  SharePost: SharePostInfo;
+  comments?: CommentInfo[];
+}
+
 export interface PostInitialState {
-  init: null;
+  posts: PostInfo[];
+  post: PostInfo | null;
 }
 
 const initialState: PostInitialState = {
-  init: null,
+  posts: [],
+  post: null,
 };
 
 const post = (state: PostInitialState = initialState, action: any) => {
