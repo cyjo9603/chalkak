e01fee71aa08c9c3fb5077db281291856370f0a9
Chanyeong Cho <cyjo9603@naver.com>
2020-04-15 20:10:53 +0900

    [feat] show profile page post

--- front/src/pages/profile.tsx
+++ front/src/pages/profile.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useRef, useCallback, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NextPageContext } from 'next';
 
 import ProfileForm from '../component/ProfileForm';
 import PostCard from '../component/PostCard';
 import { RootState } from '../reducers';
+import { getUserPostsRequest } from '../reducers/post/getUserPosts';
 
 const Profile = () => {
+  const dispatch = useDispatch();
+  const { id } = useSelector((state: RootState) => state.user.info);
   const { posts } = useSelector((state: RootState) => state.post);
+  const { hasMorePost } = useSelector((state: RootState) => state.post);
+  const countRef = useRef<string[]>([]);
+
+  const onScroll = useCallback(() => {
+    if (
+      posts.length !== 0 &&
+      hasMorePost &&
+      window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300
+    ) {
+      const lastUpdatedAt = posts[posts.length - 1].updatedAt;
+      if (!countRef.current.includes(lastUpdatedAt)) {
+        dispatch(getUserPostsRequest(id, lastUpdatedAt));
+        countRef.current.push(lastUpdatedAt);
+      }
+    }
+  }, [countRef.current, hasMorePost, posts]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+
+    return () => {
+      countRef.current = [];
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [hasMorePost, posts.length]);
 
   return (
     <>
@@ -16,6 +45,13 @@ const Profile = () => {
       ))}
     </>
   );
+};
+
+Profile.getInitialProps = async (context: NextPageContext) => {
+  const { id } = context.store.getState().user.info;
+  if (id) {
+    context.store.dispatch(getUserPostsRequest(id));
+  }
 };
 
 export default Profile;
