3ca1f2fd177def400c0025f2a8c75a749bebefb9
Chanyeong Cho <cyjo9603@naver.com>
2020-04-16 16:29:48 +0900

    [refactor] profile form optimize

--- front/src/component/ProfileForm/index.tsx
+++ front/src/component/ProfileForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { useSelector } from 'react-redux';
 import { Avatar, Row, Col, Statistic } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
@@ -6,36 +6,42 @@ import { UserOutlined } from '@ant-design/icons';
 import FormWrapper from './styled';
 import { RootState } from '../../reducers';
 
-const ProfileForm = () => {
-  const { info } = useSelector((state: RootState) => state.user);
+const ProfileForm = memo(() => {
+  const { profilePhoto } = useSelector((state: RootState) => state.user.info);
+  const { familyName } = useSelector((state: RootState) => state.user.info);
+  const { firstName } = useSelector((state: RootState) => state.user.info);
+  const { birth } = useSelector((state: RootState) => state.user.info);
+  const { phone } = useSelector((state: RootState) => state.user.info);
+  const { mail } = useSelector((state: RootState) => state.user.info);
+  const { friends } = useSelector((state: RootState) => state.user.info);
 
   return (
     <FormWrapper>
-      {info.profilePhoto ? (
-        <Avatar src={`http://localhost:3065/${info.profilePhoto}`} size={200} />
+      {profilePhoto ? (
+        <Avatar src={`http://localhost:3065/${profilePhoto}`} size={200} />
       ) : (
         <Avatar icon={<UserOutlined />} size={200} />
       )}
 
       <Row gutter={16}>
         <Col span={24}>
-          <Statistic title="이름" value={`${info.familyName} ${info.firstName}`} />
+          <Statistic title="이름" value={`${familyName} ${firstName}`} />
         </Col>
         <Col span={12}>
-          <Statistic title="생일" value={`${info.birth}`} />
+          <Statistic title="생일" value={`${birth}`} />
         </Col>
         <Col span={12}>
-          <Statistic title="연락처" value={`${info.phone} `} />
+          <Statistic title="연락처" value={`${phone} `} />
         </Col>
         <Col span={12}>
-          <Statistic title="이메일" value={`${info.mail}`} />
+          <Statistic title="이메일" value={`${mail}`} />
         </Col>
         <Col span={12}>
-          <Statistic title="친구" value={`${info.friends}명`} />
+          <Statistic title="친구" value={`${friends}명`} />
         </Col>
       </Row>
     </FormWrapper>
   );
-};
+});
 
 export default ProfileForm;
