21244a1de853fb4ef3cb5a635a7be529e7a99aca
Chanyeong Cho <cyjo9603@naver.com>
2020-04-16 16:32:24 +0900

    [refactor] userinfo optimize

--- front/src/component/UserInfo/index.tsx
+++ front/src/component/UserInfo/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { useSelector } from 'react-redux';
 import { Avatar, Row, Col, Statistic } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
@@ -6,33 +6,37 @@ import { UserOutlined } from '@ant-design/icons';
 import { RootState } from '../../reducers';
 import UserInfoWrapper from './styled';
 
-const UserInfo = () => {
-  const { otherUserInfo } = useSelector((state: RootState) => state.user);
+const UserInfo = memo(() => {
+  const { familyName } = useSelector((state: RootState) => state.user.otherUserInfo);
+  const { firstName } = useSelector((state: RootState) => state.user.otherUserInfo);
+  const { Posts } = useSelector((state: RootState) => state.user.otherUserInfo);
+  const { Friends } = useSelector((state: RootState) => state.user.otherUserInfo);
+  const { profilePhoto } = useSelector((state: RootState) => state.user.otherUserInfo);
 
   return (
     <UserInfoWrapper>
       <div>
         <Row>
           <Col span={24}>
-            <Statistic title="이름" value={`${otherUserInfo.familyName}${otherUserInfo.firstName}`} />
+            <Statistic title="이름" value={`${familyName}${firstName}`} />
           </Col>
           <Col span={12}>
-            <Statistic title="게시글" value={`${otherUserInfo.Posts}개`} />
+            <Statistic title="게시글" value={`${Posts}개`} />
           </Col>
           <Col span={12}>
-            <Statistic title="친구" value={`${otherUserInfo.Friends}명`} />
+            <Statistic title="친구" value={`${Friends}명`} />
           </Col>
         </Row>
       </div>
       <div>
-        {otherUserInfo.profilePhoto ? (
-          <Avatar size={120} src={`http://localhost:3065/${otherUserInfo.profilePhoto}`} />
+        {profilePhoto ? (
+          <Avatar size={120} src={`http://localhost:3065/${profilePhoto}`} />
         ) : (
           <Avatar size={120} icon={<UserOutlined />} />
         )}
       </div>
     </UserInfoWrapper>
   );
-};
+});
 
 export default UserInfo;
