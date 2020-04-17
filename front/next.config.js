0f7beb565166bd80517e1291564a4b10e18a060e
Chanyeong Cho <cyjo9603@naver.com>
2020-04-17 17:02:31 +0900

    [env] set bundle analyzer

--- front/next.config.js
+++ front/next.config.js
@@ -1,3 +1,4 @@
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 const withLess = require('@zeit/next-less');
 const lessToJS = require('less-vars-to-js');
 const fs = require('fs');
@@ -9,12 +10,32 @@ if (typeof require !== 'undefined') {
   require.extensions['.less'] = (file) => {};
 }
 
-module.exports = withLess({
-  lessLoaderOptions: {
-    javascriptEnabled: true,
-    modifyVars: themeVariables,
-  },
-  webpack: (config) => {
-    return config;
-  },
-});
+module.exports = withBundleAnalyzer(
+  withLess({
+    lessLoaderOptions: {
+      javascriptEnabled: true,
+      modifyVars: themeVariables,
+    },
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: 'static',
+        reportFilename: '../../bundles/server.html',
+      },
+      browser: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/client.html',
+      },
+    },
+    webpack: (config) => {
+      const prod = process.env.NODE_ENV === 'production';
+
+      return {
+        ...config,
+        mode: prod ? 'production' : 'development',
+        devtool: prod ? 'hidden-soure-map' : 'eval',
+      };
+    },
+  }),
+);
