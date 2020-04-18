57806e9e24e8a9fa435c9435c2b80d40452415ee
Chanyeong Cho <cyjo9603@naver.com>
2020-04-18 18:42:43 +0900

    [feat] auto login

--- front/src/sagas/user/signup.ts
+++ front/src/sagas/user/signup.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { takeLatest, put, call } from 'redux-saga/effects';
 
 import { SignUpRequest, SignUpData, SIGNUP_REQUEST, signUpSuccess, signUpFailure } from '../../reducers/user/signup';
+import { signInRequest } from '../../reducers/user/signin';
 
 function signUpAPI(signUpData: SignUpData) {
   return axios.post('/user/signup', signUpData);
@@ -11,6 +12,8 @@ function* signUp(action: SignUpRequest) {
   try {
     yield call(signUpAPI, action.data);
     yield put(signUpSuccess());
+    const { userId, password } = action.data;
+    yield put(signInRequest({ userId, password }));
   } catch (e) {
     console.error(e);
     yield put(signUpFailure(e));
